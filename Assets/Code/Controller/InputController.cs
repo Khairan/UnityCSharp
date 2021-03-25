d035803740f3f1ae47da6b4f01670ea57f3d11df
Khairan <alphakhairan@gmail.com>
2021-03-25 17:54:16 +0300

    Lesson7

---
+++ Assets/Code/Controller/InputController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+
+namespace RollABall
+{
+    public sealed class InputController : IExecute
+    {
+        #region Fields
+
+        private readonly PlayerBase _playerBase;
+
+        #endregion
+
+        
+        #region Methods
+
+        public InputController(PlayerBase player)
+        {
+            _playerBase = player;
+        }
+
+        public void Execute()
+        {
+            _playerBase.Move(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+        }
+
+        #endregion
+    }
+}
